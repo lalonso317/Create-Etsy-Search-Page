6fa42f60978ff96f05812420bef5654d1eef65c8
Gera.Alonso <gera.alonso0529@gmail.com>
2020-09-12 13:13:45 -0700

    pushing these changes

--- main.js
+++ main.js
@@ -3,24 +3,32 @@ const html = items.results.map(item =>{
     return `
     
     <div class="products">
-     <ul class="productlist">
+        <a href="${item.url}" target="_blank">
+        <ul class="productlist">
         <li class="specific" id="evenmore">
         <div id="fullheart">
-        <img src="${item.Images[0].url_fullxfull}"/>
+        <img class="javaimg"src="${item.Images[0].url_fullxfull}"/>
         <div id="heart">
                     <a href="#" class="hearta"><i class="fa fa-heart"></i></a>
                 </div>
         </div>
-            <h3>${item.title}</h3>
-                <div class="descr">   
-                    <p>Views: ${item.views}</p>
-                    <p class="price">&dollar;${item.price}</p>
-                </div>
-            </li>
+        <a href="${item.url}" target="_blank">
+                <h3>${item.title}</h3>
+                    <div class="descr">   
+                        <p>Views: ${item.views}</p>
+                        <div class="rating">
+                        <div class="rate"></div> 
+                        </div>
+                        <p class="price">&dollar;${item.price}</p>
+                    </div>
+                </li>
+                </a>
+            </a>
         </ul>
     </div> 
     `
 }).join('')
 document.querySelector("#allproducts").innerHTML = html
+
 
 
